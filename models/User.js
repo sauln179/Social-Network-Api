a5642c45c2237dfdee7337ced9bd9b5dfa3d6983
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-05-29 09:24:35 -0700

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        pattern: "^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$"
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref:'Thought'
+    }],
+    friends: [{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    }]
+    
+        
+},
+{
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false
+  }
+)
+//gets total of friends.
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+// create the Users model using the Users Schema
+const User = model('User', userSchema);
+
+// Export User module
+module.exports = User;
