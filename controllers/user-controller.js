77047d48c3e7b680ffcd900f4b904c134f3aee11
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-05-29 09:54:20 -0700

    Setup GET user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,15 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+
+    // GET /api/users
+    getAllUsers(req, res) {
+        User.find({})
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
 
