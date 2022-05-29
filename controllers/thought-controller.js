f90f32fbc7b6f65264032721180c3139c8c9334e
sauln179 <79173328+sauln179@users.noreply.github.com>
2022-05-29 09:55:21 -0700

    Setup GET thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,1 +1,16 @@
 
+const { User, Thought, Reaction } = require('../models');
+
+const thoughtController = {
+
+    // GET /api/thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .populate({ path: 'reactions', select: '-__v' })
+        .select('-__v')
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
